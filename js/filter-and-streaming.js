cc8257e518258e4defb4ed7ad6cb567b0769e945
Kirill Zhuravlev <kirillyz@gmail.com>
2014-10-12 04:55:41 +0200

    Tweets on the map. First version with real data.

---
+++ js/filter-and-streaming.js
@@ -0,0 +1,61 @@
+
+
+var socket = null;
+var tweets = [];
+
+var streamTweets = function(keyword){
+    if (socket != null){
+        socket.close();
+    }
+    socket = new WebSocket("ws://claire.snya.li:9000/tweets/");
+
+    var send = function (message, callback) {
+        waitForConnection(function () {
+            socket.send(message);
+            if (typeof callback !== 'undefined') {
+                callback();
+            }
+        }, 100);
+    };
+
+    var waitForConnection = function (callback, interval) {
+        if (socket.readyState === 1) {
+            callback();
+        } else {
+            setTimeout(function () {
+                waitForConnection(callback);
+            }, interval);
+        }
+    };
+
+
+    socket.onmessage = function(event) {
+        var tweet = JSON.parse(event.data);
+        tweets.push(tweet);
+        addTweet(tweet);
+        //console.log(tweets.length);
+    };
+
+    socket.onopen = function(){
+        console.log("ws open");
+    };
+
+    socket.onerror = function(err){
+        console.log("ws error",err);
+    };
+
+    socket.onclose = function(){
+        console.log("ws close");
+    };
+
+    send(keyword);
+};
+
+d3.select("#play")
+    .on("click",function(){
+        heatmapMinus.clearData();
+        heatmapMinus.update();
+        heatmapPlus.clearData();
+        heatmapPlus.update();
+        streamTweets(d3.select("#keyword").node().value);
+    });
--- js/map-and-tweets.js
+++ js/map-and-tweets.js
@@ -9,16 +9,16 @@ L.tileLayer('https://a.tiles.mapbox.com/v3/xevil.jo1368ad/{z}/{x}/{y}.png', {
 
 // Drawing of the tweets
 var heatmapPlus = new L.TileLayer.WebGLHeatMap({
-    size: 3000000,
-    opacity:0.75,
-    alphaRange: 0.1,
-    gradientTexture: "./img/blue-green.png"
-});
-var heatmapMinus = new L.TileLayer.WebGLHeatMap({
-    size: 3000000,
+    size: 1200000,
     opacity:0.75,
     alphaRange: 0.1,
     gradientTexture: "./img/red-yellow.png"
+});
+var heatmapMinus = new L.TileLayer.WebGLHeatMap({
+    size: 1200000,
+    opacity:0.75,
+    alphaRange: 0.1,
+    gradientTexture: "./img/blue-green.png"
 });
 map.addLayer(heatmapMinus);
 map.addLayer(heatmapPlus);
@@ -46,11 +46,35 @@ var requestFakeTweet = function(){
         }
     }, 50+Math.random()*100);
 };
-requestFakeTweet();
+//requestFakeTweet();
+
+// Decay
 setInterval(function(){
-    heatmapPlus.multiply(0.975);
-    heatmapMinus.multiply(0.975);
+    heatmapPlus.multiply(0.9);
+    heatmapMinus.multiply(0.9);
     var smallOnesFilter = function(d){return d[2]>0.01;};
     heatmapPlus.data = heatmapPlus.data.filter(smallOnesFilter);
     heatmapMinus.data = heatmapMinus.data.filter(smallOnesFilter);
 },40);
+
+var addTweet = function(tweet){
+
+    var point = {value:10};
+    if (tweet.coordinates != null){
+        point.long = tweet.coordinates.coordinates[0];
+        point.lat = tweet.coordinates.coordinates[1];
+    }else if (tweet.place != null && typeof tweet.place.bounding_box != "undefined"){
+        var bbc = tweet.place.bounding_box.coordinates;
+        point.long = 0.25*d3.sum(bbc,function(d){return d[0]});
+        point.lat = 0.25*d3.sum(bbc,function(d){return d[1]});
+    }else{
+        //console.log("no geo point;");
+        return;
+    }
+
+    //console.log(point);
+
+    var heatmap = (Math.random() > 0.3) ? heatmapPlus : heatmapMinus;
+    heatmap.addDataPoint(point.lat,point.long,point.value*100);
+    heatmap.update();
+};
